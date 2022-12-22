ba1eb45418a185dfedf3149754bf7884ea5624c3
Yogita Misal <yogitamisal12@gmail.com>
2022-12-22 16:12:36 +0530

    updated functional programming

--- MapFunction.js
+++ MapFunction.js
@@ -1,8 +1,16 @@
 const m1 = [2,4,3,2,5];
 
-const ans = m1.map(function arr1(x)
+const ans = m1.map(function arr1(x)   //higher order function
 {
 return x.toString(2);
 
 });
 console.log(ans);
+
+
+const m2 =[2,3,4,12,2];
+function arr1(x)
+{
+    return x.toString(2)
+}
+const op = 
