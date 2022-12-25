808420da0f2776a86c69c25988bfbef1edc9921b
Yogita Misal <yogitamisal12@gmail.com>
2022-12-25 21:06:53 +0530

    Updated prompt

---
+++ play1.js
@@ -0,0 +1,18 @@
+// var myinputarr = [];
+// var size = 5; // Array size
+ 
+// for(var a=0; a<size; a++) 
+// {
+     
+     
+//     myinputarr[a] = prompt('Enter array Element ' + (a+1));
+// }
+ 
+// console.log(myinputarr);
+var input =[];
+var size=5;
+for(var i=0;i<size;i++)
+{
+    input[i]=prompt('Enter array Element ' + (i+1));
+}
+console.log(input)
