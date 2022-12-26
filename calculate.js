d82d8aec818a4b71483666d0eafc3af49c27fcc9
Yogita Misal <yogitamisal12@gmail.com>
2022-12-26 13:06:42 +0530

    Updated promp-input

--- calculate.js
+++ calculate.js
@@ -1,11 +1,13 @@
+//How to take input from user
 const prompt = require("prompt-sync")();
 let a = prompt("enter a number: ")
 console.log(a)
 function add()
 {
     console.log("Addition of two number is ")
-    var a = prompt("enter the first number")
-    var b = prompt("enter the second number")
+    var a = parseInt(prompt("enter the first number"))
+    var b = parseInt(prompt("enter the second number"))
+    prompt(typeof a)
     console.log(a+b)
 
 
