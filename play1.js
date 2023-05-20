5eeb9c92b0ce9ac5b4e1bc5ce9c8c28a2a20cfb6
yogita_misal <yogitamisal12@gmail.com>
2023-05-20 09:26:52 +0530

    added map func and null udefined meaning

--- play1.js
+++ play1.js
@@ -1,6 +1,15 @@
 // var myinputarr = [];
 // var size = 5; // Array size
  
+
+
+
+
+
+
+
+
+
 // for(var a=0; a<size; a++) 
 // {
      
@@ -9,10 +18,53 @@
 // }
  
 // console.log(myinputarr);
-var input =[];
-var size=5;
-for(var i=0;i<size;i++)
+// var input =[];
+// var size=2;
+// for(var i=0;i<size;i++)
+// {
+//     input[i]=prompt('Enter array Element ' + (i+1));
+// }
+// console.log(input)
+// console.log("Helloooo")
+//Diffrence between foreach and map
+
+const arr = [1,2,3,4,5];
+//const output = arr.map((x)=> console.log(x));
+
+
+function myFunc(number)
 {
-    input[i]=prompt('Enter array Element ' + (i+1));
+    return number*3;
 }
-console.log(input)
+console.log(arr.map(myFunc)) //we can add filter method after myFunc
+
+
+//vs forEach()
+const foreachres = arr.forEach((x)=>
+{
+    return x*2;
+});
+console.log(foreachres)
+
+
+//null and undefined diff
+
+//null is actual value type is object
+//undefined is var is yet to initilized ,not defined
+console.log(null==undefined)//true
+console.log(null===undefined)//false 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
